566f5e1011e81d92545ae4287f8fbaefeb1b5eab
Lars Mühmel <larsmuehmel@web.de>
2021-05-21 15:46:43 +0200

    moved to naersk

--- flake.nix
+++ flake.nix
@@ -1,46 +1,46 @@
 {
   description = "A very basic flake";
 
-  inputs.import-cargo.url = "github:edolstra/import-cargo";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs, import-cargo }:
-    let
-      build = release:
-
-        let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          importCargo = import-cargo.builders.importCargo;
-          desktopFile = pkgs.makeDesktopItem {
-            name = "wgpu-mandelbrot";
-            exec = "@out@/bin/wgpu-mandelbrot";
-            comment = "A simple mandelbrot renderer using wgpu";
-            desktopName = "Mandelbrot";
-            categories = "Game;";
-          };
-          libPath = pkgs.lib.makeLibraryPath [
-            pkgs.libxkbcommon
-            pkgs.vulkan-loader
-            pkgs.wayland
-          ];
-          cargo-import = (importCargo {
-            lockFile = ./Cargo.lock;
-            inherit pkgs;
-          });
-        in pkgs.stdenv.mkDerivation {
+  outputs = { self, nixpkgs, fenix, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        desktopFile = pkgs.makeDesktopItem {
           name = "wgpu-mandelbrot";
+          exec = "@out@/bin/wgpu-mandelbrot";
+          comment = "A simple mandelbrot renderer using wgpu";
+          desktopName = "Mandelbrot";
+          categories = "Game;";
+        };
+        libPath = pkgs.lib.makeLibraryPath [
+          pkgs.libxkbcommon
+          pkgs.vulkan-loader
+          pkgs.wayland
+        ];
+        build = (naersk.lib.${system}.override {
+          inherit (fenix.packages.${system}.minimal) cargo rustc;
+        }).buildPackage {
+          name = "wgpu-mandelbrot";
+          version = "0.1.0";
           src = self;
-
-          dontUseCmakeConfigure = true;
-
           nativeBuildInputs = [
-            cargo-import.cargoHome
-
-            pkgs.rustc
-            pkgs.cargo
+            pkgs.makeWrapper
             pkgs.pkgconfig
           ];
           buildInputs = [
-            pkgs.makeWrapper
             pkgs.xorg.libX11
             pkgs.xorg.libXrandr
             pkgs.xorg.libXi
@@ -48,34 +48,26 @@
             pkgs.mesa
             pkgs.vulkan-loader
           ];
-          buildPhase = ''
-            cargo build --offline ${if release then "--release" else ""}
-          '';
-          installPhase = ''
-            install -Dm775 ./target/${
-              if release then "release" else "debug"
-            }/wgpu-mandelbrot $out/bin/wgpu-mandelbrot
+          SHADERC_LIB_DIR = pkgs.shaderc.static + "/lib";
+          overrideMain = old: old // {
+            postInstall = ''
+              wrapProgram $out/bin/wgpu-mandelbrot --suffix LD_LIBRARY_PATH : "${libPath}"
 
-            wrapProgram $out/bin/wgpu-mandelbrot --suffix LD_LIBRARY_PATH : "${libPath}"
-
-            mkdir -p $out/share/applications
-            substituteAll ${desktopFile}/share/applications/wgpu-mandelbrot.desktop \
-              $out/share/applications/wgpu-mandelbrot.dekstop
-          '';
-
-          shellHook = ''
-            export CARGO_HOME=${cargo-import.vendorDir}/vendor
-            export LD_LIBRARY_PATH="${libPath}";
-          '';
-          SHADERC_LIB_DIR = "${pkgs.shaderc.static}/lib";
+              mkdir -p $out/share/applications
+              substituteAll ${desktopFile}/share/applications/wgpu-mandelbrot.desktop \
+                $out/share/applications/wgpu-mandelbrot.dekstop
+            '';
+          };
         };
-    in {
 
-      packages.x86_64-linux = {
-        debug = build false;
-        release = build true;
-      };
-
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.release;
-    };
+      in
+      {
+        packages.wgpu-mandelbrot = build;
+        defaultPackage = self.packages.${system}.wgpu-mandelbrot;
+        devShell = pkgs.mkShell {
+          inputsFrom = [ self.defaultPackage.${system} ];
+          buildInputs = [ pkgs.crate2nix ];
+        };
+      }
+    );
 }
